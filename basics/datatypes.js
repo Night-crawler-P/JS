4d506fb9d4bc09370e6887576c5eca62baedaf75
Night-crawler-P <parshwashah055@gmail.com>
2025-06-17 09:30:42 +0000

    dtypes

---
+++ basics/datatypes.js
@@ -0,0 +1,29 @@
+// "use script" used for treating all JS code as of new version
+// alert() for browser not node
+
+// ecma used for defining standards
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
