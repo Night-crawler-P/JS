a4693f69ed9a026257c196a66132cfbb1131c90d
Night-crawler-P <parshwashah055@gmail.com>
2025-06-17 09:22:08 +0000

    let,var,const

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+// Intro to let,var,const
+const acc_no=123;
+let name="Parshwa"
+var adress="NYC"
+console.table([acc_no,name,adress])
+// trying to change const value
+// acc_no=234;//Error returned
+
+name="PAR"
+adress="XYZ"
+console.table([acc_no,name,adress])
+
+/*
+Var must never be used as it does not understand the concept of scope.
+*/
